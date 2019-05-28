85fe7d78fb484369c051f5b738279778c955e29c
Nate-Mock <nate.r.mock@gmail.com>
2019-05-28 11:56:07 +0000

    Changing the line length for the second description.

--- two-sum.py
+++ two-sum.py
@@ -1,4 +1,5 @@
 """
+Problem Statement:
 Given an array of integers, return indicies of the two numbers such that they add up to a specific target.
 You may assume each input would have exactly one solution, and you may not use the same element twice.
 """
@@ -12,8 +13,11 @@ class Solution:
 			if search in nums and nums.index(comp) != i:
 				return [i, nums.index(comp)]
 
-
-# After submitting my solution, I read through some of the discussions and found a /much/ more ingenious solution using a dictionary with the keys as the compliment and the values as the original list index. 
+"""
+After submitting my solution, I read through some of the discussions and found a
+/much/ more ingenious solution using a dictionary with the keys as the compliment 
+and the values as the original list index. This is my rewrite:
+""" 
 # Runtime: 32 ms (faster than 99.12% of python3 submissions)
 # Memory Usage 14.5 MB (better than 23.17% of python3 submissions)
 class Solution:
@@ -21,5 +25,5 @@ class Solution:
 		m = {}
 		for i, x in enumerate(nums):
 			if x in m:
-				return [m[x], i]
+				return [m[x], i] 
 			m[target - x] = i
