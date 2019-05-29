f4baf015ff1f2ea55d236d0c969eeea877ace0b4
Nate-Mock <nate.r.mock@gmail.com>
2019-05-29 23:15:42 +0000

    My second Leet-Code Python submission!

--- reverse-integer.py
+++ reverse-integer.py
@@ -35,3 +35,18 @@ class Solution:
 		if r <= -2**31 or r >= 2**31 - 1:
 			return 0
 		return r
+
+# Looking to the discussion section, I found a golfer! Here was his solution:
+Class Solution:
+	def reverse(self, x):
+		s = cmp(x,0)
+		r = int(`s*x`[::-1])
+		return s*r * (r < 2**31)
+
+# One thing I notice here - beyond the fact that this code is 1/4 the length
+# of my own - is that Stefan (the author) simply checks if r is less than 2**31.
+# Initially, I was concerned this would leave out the edge case of when
+# r == 2**31 and s*r == -2**31, a valid 32 bit signed integer, 
+# however after some consideration I realized that's not possible:
+# in order for r to equal -2147483648 (-2**31), x would have to be
+# -8463847412, which isn't a valid input given the 32 bit constraint. 
